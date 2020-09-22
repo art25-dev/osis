ba02a889edc6dce25512c4c3e9178624c90e7675
Артем Гаврилюк <art25@MacBook-Pro-Artem.local>
2020-09-22 07:24:07 +0300

    add logs

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,74 @@
 <template>
-  <div class="wrapper">
+  <!-- <div class="wrapper">
     <h1>Внимание!</h1>
     <p>"Открытая справочная инфорационная система" работает в тестовом режиме.</p>
     <p>Скачивание антивирусных баз временно недоступно.</p>
+  </div>-->
+  <div>
+    <svg-icon @click="showPrev()" class="menu__controls-btn" name="arrow" />
+    <VueSlickCarousel ref="carousel" v-bind="settings">
+      <div>
+        <img src="https://picsum.photos/300/300" />
+      </div>
+      <div>
+        <img src="https://picsum.photos/300/300" />
+      </div>
+      <div>
+        <img src="https://picsum.photos/300/300" />
+      </div>
+    </VueSlickCarousel>
+    <svg-icon @click="showNext()" class="menu__controls-btn" name="arrow" />
   </div>
 </template>
 
 <script>
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+// optional style for arrows & dots
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+
 export default {
   head: {
-    title: "OSIS"
+    title: "OSIS",
   },
-  components: {},
-  layout: "user"
+  components: {
+    VueSlickCarousel,
+  },
+  layout: "user",
+  data() {
+    return {
+      settings: {
+        arrows: false,
+        dots: true,
+        fade: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    };
+  },
+  methods: {
+    showNext() {
+        this.$refs.carousel.next()
+    },
+    showPrev() {
+        this.$refs.carousel.prev()
+    },
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-  .wrapper {
-    text-align: center;
-    margin: 3rem;
-  }
-  h1 {
-    color: $color-danger;
-    font-size: 52px;
-  }
-  p {
-    font-size: 24px;
-  }
+.wrapper {
+  text-align: center;
+  margin: 3rem;
+}
+h1 {
+  color: $color-danger;
+  font-size: 52px;
+}
+p {
+  font-size: 24px;
+}
 </style>
--- plugins/globals.js
+++ plugins/globals.js
@@ -6,6 +6,8 @@ import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
+
+
 export default function ({ $moment }) {
   $moment.locale("ru");
 }
@@ -16,6 +18,9 @@ config.autoAddCss = false
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
 library.add(fas)
+
+
+
 
 // Register the component globally
 Vue.component('font-awesome-icon', FontAwesomeIcon)
